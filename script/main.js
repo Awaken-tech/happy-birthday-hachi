a3422dae2b4d150d40f27776b319c0d2f42ba20d
Awaken-tech <52420077+Awaken-tech@users.noreply.github.com>
2024-09-07 03:58:21 +0800

    Update main.js

--- script/main.js
+++ script/main.js
@@ -110,7 +110,7 @@ const animationTimeline = () => {
     })
     .staggerTo(
       ".hbd-chatbox span",
-      0.5,
+      1,
       {
         visibility: "visible"
       },
@@ -223,7 +223,7 @@ const animationTimeline = () => {
       "-=2"
     )
     .from(".hat", 0.5, {
-      x: -100,
+      x: -150,
       y: 350,
       rotation: -180,
       opacity: 0
